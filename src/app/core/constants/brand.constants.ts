4d23d9d0bbe317eb867b3c2a69cb06825fdaa7e5
shubham390-liquilogic <shubham.shrivastav@liquilogic.in>
2026-08-02 09:24:09 +0530

    Logo added

--- src/app/core/constants/brand.constants.ts
+++ src/app/core/constants/brand.constants.ts
@@ -2,4 +2,4 @@ export const APP_LOGO_SRC = '/images/PropnutlogoBlue.png';
 export const APP_LOGO_SRC_Dark = '/images/propnutlogoDark.png';
 
 
-export const APP_RAIL_LOGO_SRC = '/images/Light_Logo.jpg';
+export const APP_RAIL_LOGO_SRC = '/images/Light_Logo.png';
